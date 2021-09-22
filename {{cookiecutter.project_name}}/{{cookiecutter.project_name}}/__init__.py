59376235a9b555cd36748cdccdd0d7b1814c07fb
Pierre Delaunay <pierre.delaunay.tr@gmail.com>
2021-09-22 14:30:10 -0400

    Add missing test dependencies

--- {{cookiecutter.project_name}}/{{cookiecutter.project_name}}/__init__.py
+++ {{cookiecutter.project_name}}/{{cookiecutter.project_name}}/__init__.py
@@ -6,3 +6,37 @@ __author_short__ = u'{{cookiecutter.author_short}}'
 __author_email__ = '{{cookiecutter.email}}'
 __copyright__ = u'{{cookiecutter.copyright}} {{cookiecutter.author}}'
 __url__ = '{{cookiecutter.url}}'
+
+
+
+
+def my_function(a: int, b: int) -> int:
+    """Add two numbers together
+
+    Parameters
+    ----------
+    a: int
+        first integer
+
+    b: int
+        second integer
+
+    Raises
+    ------
+    value errror if a == 0 
+
+    Examples
+    --------
+
+    >>> my_function(1, 2)
+    3
+    >>> my_function(0, 2)
+    Traceback (most recent call last):
+      ...
+    ValueError
+
+    """
+    if a == 0:
+        raise ValueError()
+
+    return a + b
