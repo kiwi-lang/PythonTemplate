c07095a7e5cecbca441be2c3c809211f8ed767c0
Setepenre <pierre.delaunay@outlook.com>
2021-12-06 17:42:55 -0500

    Remove project_brief and author short

--- {{cookiecutter.project_name}}/{{cookiecutter.project_name}}/__init__.py
+++ {{cookiecutter.project_name}}/{{cookiecutter.project_name}}/__init__.py
@@ -2,7 +2,6 @@ __descr__ = '{{cookiecutter.description}}'
 __version__ = '{{cookiecutter.version}}'
 __license__ = '{{cookiecutter.license}}'
 __author__ = u'{{cookiecutter.author}}'
-__author_short__ = u'{{cookiecutter.author_short}}'
 __author_email__ = '{{cookiecutter.email}}'
 __copyright__ = u'{{cookiecutter.copyright}} {{cookiecutter.author}}'
 __url__ = '{{cookiecutter.url}}'
@@ -23,7 +22,7 @@ def my_function(a: int, b: int) -> int:
 
     Raises
     ------
-    value errror if a == 0 
+    value errror if a == 0
 
     Examples
     --------
